940574514fb1510633f6ebc7bbfb91a8a73352b2
Danilo Guimarães <danilogguimaraes@outlook.com>
2025-11-22 18:46:45 +0000

    feat: define application routes for CompaniesController with root and collection actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "companies#index"
+  
+  resources :companies, only: [:index] do
+    collection do
+      get :admin
+      post :import
+    end
+  end
 end
