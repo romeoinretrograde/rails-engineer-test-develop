875921ea833121550b845ae0d5f7b80d62d8d9ba
Danilo Guimarães <danilogguimaraes@outlook.com>
2025-11-22 18:53:10 +0000

    feat: remove unnecessary console logs from search controller

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,15 +4,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form"]
 
-  connect() {
-    console.log("Search controller connected")
-  }
-
   submit() {
     console.log("Submit called")
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
-      console.log("Submitting form")
       const form = this.element.querySelector('form')
       if (form) {
         form.requestSubmit()
